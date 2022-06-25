f0e7fb06e9d5e3f5cf02d2e265b38f8843fee610
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-06-25 17:33:52 +0530

    Javascript basics added.

---
+++ 00_Javascript-Fundamentals/02_Basics.js
@@ -0,0 +1,8 @@
+// This is how we do comment in Javascript file.
+
+let x, y, z;  // Variable declaration
+x = 5;        // Variable assignment
+y = 6;        
+z = x + y;    // Arithmatic operation
+
+console.log('x:',x,'y:',y,'z:',z);  // Printing in terminal

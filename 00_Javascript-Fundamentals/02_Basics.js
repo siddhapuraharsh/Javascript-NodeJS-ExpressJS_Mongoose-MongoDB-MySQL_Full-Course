00f2427ef92969863abcf45281ceacf831f1cd28
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-06-25 19:25:21 +0530

    Fundamentals updated.

--- 00_Javascript-Fundamentals/02_Basics.js
+++ 00_Javascript-Fundamentals/02_Basics.js
@@ -4,5 +4,22 @@ let x, y, z;  // Variable declaration
 x = 5;        // Variable assignment
 y = 6;        
 z = x + y;    // Arithmatic operation
-
 console.log('x:',x,'y:',y,'z:',z);  // Printing in terminal
+
+
+// Difference between == and ===
+let a = 3;
+let b = '3';
+console.log('\na==b : ',a==b);    // Compares just value
+console.log('a===b : ',a===b);   // Compares values and type
+
+
+
+// Array fundamentals
+let arr = [0,1,2,3,4,5,4,3,2,1,0];
+
+const result = arr.filter((item)=>{
+    return item>=4;
+});
+
+console.warn('\nFiltered value from array : ',result);
--- 01_NodeJS-Fundamentals/fundamentals.js
+++ 01_NodeJS-Fundamentals/fundamentals.js
@@ -1,15 +1,9 @@
 // Export import variables
 const exportedVariables = require('./export-variables')
-
 console.log('Exported function from other file : ',exportedVariables.sumXY())
 
 
-
-// Array fundamentals
-let arr = [0,1,2,3,4,5,4,3,2,1,0];
-
-const result = arr.filter((item)=>{
-    return item>=4;
-});
-
-console.warn('Filtered value from array : ',result);
+// Core Modules
+const fsVariable = require('fs');
+fsVariable.writeFileSync('Non-Global.txt','Example of Non-Global Core Module...!');
+console.log('\nNon-Global Core Module txt written.');

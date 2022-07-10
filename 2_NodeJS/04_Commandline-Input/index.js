308ee46104d789c59f0991383fcf3bfa8646ed97
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-07-10 18:40:41 +0530

    Repository updated.

--- 2_NodeJS/04_Commandline-Input/index.js
+++ 2_NodeJS/04_Commandline-Input/index.js
@@ -1,15 +1,22 @@
-// console.log(process.argv[2]);
+// console.log(process.argv);          
 
-const fs = require("fs");
+// nodemon index.js
+// nodemon index.js Add xyz.txt "This is xyz file"
+// nodemon index.js Delete xyz.txt  
 
-const input = process.argv;
+const fs = require("fs");              
 
-if (input[2] == "Add") {
-  fs.writeFileSync(input[3], input[4]);
-} 
-else if (input[2] == "Delete") {
+const input = process.argv;   // input = ['node.exe', '././index.js', 'Add', 'xyz.txt', 'This is xyz file']
+// console.log(input);
+
+if(input[2]=="Add"){
+  // we will create one file and write in that file
+  fs.writeFileSync(input[3],input[4]);
+}
+else if(input[2]=="Delete"){
+  // we are going to delete that particular file
   fs.unlinkSync(input[3]);
-} 
-else {
-  console.log("Invalid Input...!!!");
+}
+else{
+  console.log('Invalid Argument');
 }

73428022bbc631ebd71ea91a2191d57cc8ae583a
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-10-12 01:14:32 +0530

    ExpressJS PUT and DELETE methods added.

--- 3_Advanced_NodeJS-ExpressJS-Mongoose-MongoDB/03_ExpressJS-Beginner-Intermediate-Topics/index.js
+++ 3_Advanced_NodeJS-ExpressJS-Mongoose-MongoDB/03_ExpressJS-Beginner-Intermediate-Topics/index.js
@@ -36,7 +36,7 @@ app.get('/api/courses/showparams/:id', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   // 404 error is thrown when particular resource is not found
-  if (!course) res.status(404).send(`Course with id ${req.params.id} does not exist...!!!`);
+  if (!course) return res.status(404).send(`Course with id ${req.params.id} does not exist...!!!`);
   res.send(course);
 });
 
@@ -48,9 +48,8 @@ app.get('/api/courses/:id', (req, res) => {
 app.post('/api/courses', (req, res) => {
   const courseName = req.body.name;
   if (!courseName || courseName.length<3) {
-    res.status(400).send('Name is required and must be at least 3 characters long...');
     // 400 status code is thrown when it is a Bad Request or Invalid
-    return;
+    return res.status(400).send('Name is required and must be at least 3 characters long...');
   } 
   const course = {
     id: courses.length + 1,
@@ -67,16 +66,63 @@ app.post('/api/joi/courses', (req, res) => {
   });
 
   const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  if (result.error) return res.status(400).send(result.error.details[0].message);
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
   courses.push(course);
   res.send(courses);
+});
+
+
+
+
+
+// PUT =================================================================
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+  return schema.validate(course);
+}
+
+app.put('/api/courses/:id', (req,res) => {
+
+  // Look up if the course exists or not or else throw 404 error that resource does not exist
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send(`Course with ID: ${req.params.id} does not exist...`);
+
+  // Validate the request parameters, else throw 400 error as Bad Request
+  /** 
+    const result = validateCourse(req.body);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+  **/
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Update the course and return the course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+
+
+
+
+// DELETE =================================================================
+
+app.delete('/api/courses/:id', (req,res) => {
+  // Look up if the course exists, else respond 404 error
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send(`Course with ID: ${req.params.id} does not exist...`)
+
+  // Delete the course and return that course
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
 });
 
 
